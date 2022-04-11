d3c7664e879428654d1b7f82b21f6c1484f921e0
Ian <ian@support.webartdesign.com.au>
2022-04-11 13:07:47 +0800

    feat: init android multiscan

--- README.md
+++ README.md
@@ -51,6 +51,22 @@ This plugins requires the following usage descriptions:
 - `NSCameraUsageDescription` specifies the reason for your app to access the device's camera.
 - `NSPhotoLibraryUsageDescription` specifies the reason for your app to access the user's photo library.
 
+## Android Quirks
+
+- Add idscan-public maven repository to the project build.gradle file.
+
+```
+allprojects {
+    repositories {
+        ...
+        maven {
+            url 'https://www.myget.org/F/idscan-public/maven/'
+        }
+        ...
+    }
+}
+```
+
 ## Contributing
 
 See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
--- android/src/main/java/com/reactnativeidscansdk/IdscanSdkModule.java
+++ android/src/main/java/com/reactnativeidscansdk/IdscanSdkModule.java
@@ -1,21 +1,107 @@
 package com.reactnativeidscansdk;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import net.idscan.components.android.multiscan.MultiScanActivity;
+import net.idscan.components.android.multiscan.Version;
+import net.idscan.components.android.multiscan.common.DocumentData;
+import net.idscan.components.android.multiscan.components.mrz.MRZComponent;
+import net.idscan.components.android.multiscan.components.mrz.MRZData;
+import net.idscan.components.android.multiscan.components.pdf417.PDF417Component;
+import net.idscan.components.android.multiscan.components.pdf417.PDF417Data;
 
 @ReactModule(name = IdscanSdkModule.NAME)
 public class IdscanSdkModule extends ReactContextBaseJavaModule {
+    static final String typeCombined = "combined";
+    static final String typeMRZ = "mrz";
+    static final String typePDF = "pdf";
+
+    private final static int SCAN_ACTIVITY_CODE = 0x001;
+    private Callback callback;
+    private String scannerType, scannerPDFKey, scannerMRZKey, parserKey;
+
+    protected final static String[] permissions = { Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+
     public static final String NAME = "IdscanSdk";
 
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+
+      @Override
+      public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(activity, requestCode, resultCode, data);
+
+        if (requestCode == SCAN_ACTIVITY_CODE) {
+          switch (resultCode) {
+            case MultiScanActivity.RESULT_OK:
+              if (data != null) {
+                DocumentData document = (DocumentData) data.getSerializableExtra(MultiScanActivity.DOCUMENT_DATA);
+                if (document != null) {
+                  MRZData mrzData = MRZComponent.extractDataFromDocument(document);
+                  PDF417Data pdf417Data = PDF417Component.extractDataFromDocument(document);
+
+                  if (mrzData != null) {
+                    Log.d(NAME, "TODO: parse MRZ Data");
+                  }
+
+                  if (pdf417Data != null) {
+                    Log.d(NAME, "TODO: parse PDF Data");
+                  }
+                }
+              }
+              break;
+
+            case MultiScanActivity.ERROR_RECOGNITION:
+              Log.d(NAME, data.getStringExtra(MultiScanActivity.ERROR_DESCRIPTION));
+              break;
+
+            case MultiScanActivity.ERROR_INVALID_CAMERA_NUMBER:
+              Log.d(NAME, "Invalid camera number.");
+              break;
+
+            case MultiScanActivity.ERROR_CAMERA_NOT_AVAILABLE:
+              Log.d(NAME, "Camera not available.");
+              break;
+
+            case MultiScanActivity.ERROR_INVALID_CAMERA_ACCESS:
+              Log.d(NAME, "Invalid camera access.");
+              break;
+
+            case MultiScanActivity.RESULT_CANCELED:
+              break;
+
+            default:
+              Log.d(NAME, "Undefined error.");
+              break;
+          }
+        }
+      }
+    };
+
     public IdscanSdkModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+      super(reactContext);
+
+      // Add the listener for `onActivityResult`
+      reactContext.addActivityEventListener(mActivityEventListener);
     }
 
     @Override
@@ -26,13 +112,45 @@ public class IdscanSdkModule extends ReactContextBaseJavaModule {
 
     // TODO: trigger license scanner
     @ReactMethod
-    public void scan(String cameraKey, String parserKey, Callback callback) {
-        WritableMap scanResult = Arguments.createMap();
+    public void scan(String type, ReadableMap apiKeys, Callback callback) {
+        Log.d(NAME, "React Native IDScanner starting");
 
-        scanResult.putString('name', 'John Doe')
+        if (apiKeys.hasKey("androidDetectorPDFLicenseKey") && apiKeys.hasKey("androidDetectorMRZLicenseKey") && apiKeys.hasKey("androidParserPDFLicenseKey")) {
+          this.scannerType = type;
+          this.scannerMRZKey = apiKeys.getString("androidDetectorMRZLicenseKey");
+          this.scannerPDFKey = apiKeys.getString("androidDetectorPDFLicenseKey");
+          this.parserKey = apiKeys.getString("androidParserPDFLicenseKey");
+        } else {
+          WritableMap scanResult = Arguments.createMap();
+          scanResult.putString("success", "false");
+          callback.invoke("Must provide activation keys for IDScan.net's camera scanner and id parser SDKs");
 
-        callback.invoke(null, scanResult)
+          return;
+        }
+
+        this.callback = callback;
+        showDefaultScanView();
     }
 
-    public static native int nativeMultiply(int a, int b);
+    private void showDefaultScanView() {
+      MultiScanActivity.build(getCurrentActivity())
+        .withComponent(PDF417Component.build()
+          .withLicenseKey("")
+          .complete())
+        .withComponent(MRZComponent.build()
+          .withLicenseKey("")
+          .complete())
+        .start(SCAN_ACTIVITY_CODE);
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+      final Map<String, Object> constants = new HashMap<>();
+
+      constants.put("TYPE_COMBINED", "combined");
+      constants.put("TYPE_MRZ", "mrz");
+      constants.put("TYPE_PDF", "pdf");
+
+      return constants;
+    }
 }
